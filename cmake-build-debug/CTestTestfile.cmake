c7b3685113830b1d6eee5aba105043e1bce47297
pink <3213748560@qq.com>
2022-08-18 15:00:48 +0800

    Pass 2B

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pink/Public/bustub
-# Build directory: /home/pink/Public/bustub/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest-build")
-subdirs("src")
-subdirs("test")
--- cmake-build-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pink/Public/bustub/src
-# Build directory: /home/pink/Public/bustub/cmake-build-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
